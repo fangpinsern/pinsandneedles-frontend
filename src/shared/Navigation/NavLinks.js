453fec512ef8456c728c2d7b982c472a4613408d
fps <universityforfang@gmail.com>
2020-05-31 15:17:36 +0800

    Made changes to navigation bar to show on hover
    - Can now have all functions of the same header under one umbrella
    - e.g adding new projects is now under projects header

--- src/shared/Navigation/NavLinks.js
+++ src/shared/Navigation/NavLinks.js
@@ -17,21 +17,41 @@ function NavLinks(props) {
       <li>
         <NavLink to="/about">About</NavLink>
       </li>
-      <li>
-        <NavLink to="/projects">Projects</NavLink>
-      </li>
-      <li>
-        <NavLink to="/products">Products</NavLink>
-      </li>
+      {!auth.isLoggedIn ? (
+        <li>
+          <NavLink to="/projects">Projects</NavLink>
+        </li>
+      ) : (
+        <li>
+          <div className="dropdown">
+            <button className="dropbtn">Projects</button>
+            <div className="dropdown-content">
+              <NavLink to="/projects">Projects</NavLink>
+              <NavLink to="/projects/newinput">New Project</NavLink>
+              <NavLink to="/projects/update">Update Project</NavLink>
+            </div>
+          </div>
+        </li>
+      )}
+      {!auth.isLoggedIn ? (
+        <li>
+          <NavLink to="/products">Products</NavLink>
+        </li>
+      ) : (
+        <li>
+          <div className="dropdown">
+            <button className="dropbtn">Products</button>
+            <div className="dropdown-content">
+              <NavLink to="/products">Products</NavLink>
+              <NavLink to="/projects/inventory">Inventory</NavLink>
+              <NavLink to="/products/update">Update Products</NavLink>
+            </div>
+          </div>
+        </li>
+      )}
       <li>
         <NavLink to="/contact">Contact</NavLink>
       </li>
-      {auth.isLoggedIn && <li>
-        <NavLink to="/projects/newinput">New</NavLink>
-      </li>}
-      {auth.isLoggedIn && <li>
-        <NavLink to="/products/inventory">Inventory</NavLink>
-      </li>}
     </ul>
   );
 }
